90206e34943334a2921b4715bbe2ccc075a314e2
Manisha gaonkar <manishagaonkar7875@gmail.com>
2020-10-19 12:49:03 +0530

    Added Validations

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -15,8 +15,80 @@ class Log {
     }
   }
 }
+
+/**
+ * Formatting the error
+ */
+formatError = function (error, msg, code = 0) {
+  if (!code) {
+    code = 0;
+  }
+  if (!msg) {
+    msg = "Error occured.";
+  }
+  if (error) {
+    code = error.code ? error.code : code;
+    if (error.errmsg) {
+      msg = error.errmsg;
+    } else if (error.message) {
+      msg = error.message;
+    }
+  }
+  switch (code) {
+    case 11000:
+      msg = "Duplicate Entry.";
+  }
+  return {
+    error: [{
+      code: code,
+      message: msg
+    }]
+  };
+};
+
+/**
+ * Validates Email
+ */
+validateEmail = function (value) {
+  if (!validator.isEmail(value.toString().trim())) {
+    return false;
+  }
+  return true;
+};
+/**
+ * Password should be between 8 to 20 characters 
+ * contain at least one lowercase letter, 
+ * one uppercase letter, 
+ * one numeric digit, 
+ * one special character
+ */
+validatePassword = function (value) {
+  reg_exp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/);
+  if (reg_exp.test(value.toString().trim()) == false) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * Username should be between 3 to 25 characters 
+ * contain only letters and numbers
+ */
+validateUsername = function (value) {
+  reg_exp = new RegExp(/^[a-zA-Z0-9_]{3,25}$/);
+  if (reg_exp.test(value.toString().trim().toLowerCase()) == false) {
+    return false;
+  }
+  return true;
+};
+
+
 module.exports           = {
   _                      : _,
   async                  : async,
-  validator              : validator
+  formatError            : formatError,
+  validator              : validator,
+  validateEmail          : validateEmail,
+  validatePassword       : validatePassword,
+  validateUsername       : validateUsername
 };
